a28325ad0f3e11ac9f158928b034d0a15898ff81
dharmiksanthoshraghav <94697483+dharmiksanthoshraghav@users.noreply.github.com>
2022-09-05 14:42:34 +0530

    Update scrapemore.py

--- scrapemore.py
+++ scrapemore.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 
-
 bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 
 page = requests.get(bright_stars_url)
